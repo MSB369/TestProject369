328ba634916aaa575cf35bd6711eec4ab7a86970
satta <satta@SATTARBAIGH>
2024-06-20 19:39:19 +0530

    cccc

--- TestProject369/src/Testing/HelloWorld.java
+++ TestProject369/src/Testing/HelloWorld.java
@@ -1,9 +1,12 @@
 package Testing;
 
 public class HelloWorld {
+	
+	
 	public static void main(String[] args) {
+		
 		String abc = " Hello World";
-		System.out.println("HelloWorld.main()"+ abc);
+		System.out.println("HelloWorld.main()" + abc);
 	}
 
 }
