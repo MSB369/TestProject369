da2b1ac79d88b7549e8e0714963c97abbd5f5f50
satta <satta@SATTARBAIGH>
2024-06-20 19:51:19 +0530

    i did it

--- TestProject369/src/Testing/HelloWorld.java
+++ TestProject369/src/Testing/HelloWorld.java
@@ -1,12 +1,13 @@
 package Testing;
 
 public class HelloWorld {
-	
-	
+
 	public static void main(String[] args) {
-		
-		String abc = " Hello Worldllllllllllllllllll";
+
+		String abc = " Hello Worldl";
 		System.out.println("HelloWorld.main()" + abc);
+		int accc = 123456789;
+		System.out.println("HelloWorld.main()"+ accc);
 	}
 
 }
